9895177287c0ff33ecf1c7ab996b706bd9cb4de3
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 11:30:25 -0500

    index view login and conroller routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,26 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+post '/login' do
+  # authenticate?
+  # start new session
+  # direct to user page
+  # session[:user_id] = @user.id
+  redirect '/users/:user_id'
+end
+
+post '/signup' do
+  # validate and enter new user into DB
+  # start new session
+  # direct to user page
+  # session[:user_id] = @user.id
+  redirect '/users/:user_id'
+end
+
+get '/users/:user_id' do
+  # validate user is logged in?
+  # redirect to login if not
+  # if logged in render user page
+  erb :user_page
 end
